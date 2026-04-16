7a74b65ddb97d53dd914e007dafca75ab384ee70
Sara.Kolas <Sara.Kolas@optimumdataanalytics.com>
2026-04-16 12:56:29 +0530

    clean code test for quality gate

---
+++ cleancode.py
@@ -0,0 +1,12 @@
+def add_numbers(a: int, b: int) -> int:
+    """Return sum of two numbers."""
+    return a + b
+
+
+def main():
+    result = add_numbers(5, 10)
+    print(f"Result is: {result}")
+
+
+if __name__ == "__main__":
+    main()
